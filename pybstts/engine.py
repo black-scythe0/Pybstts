6be3d41c4f75fb82caff48d130a3b2be3cf781b1
black-scythe0 <scytheblack991@gmail.com>
2025-06-23 01:36:17 +0530

    engine __dir__

--- pybstts/engine.py
+++ pybstts/engine.py
@@ -1,24 +1,31 @@
 import os
-from tts import _espeak
+from tts import Exceptions
 
 class Engine:
 
     def __init__():
         pass
 
-    def init():
+    def init(): #initialize the engine, may take arguements ,
+                # if no arguements are given then figure out it self which engine to use.
+        pass      
 
-        lib = _espeak.Espeak.check_lib()
-        if lib != 1:
+    def get_info():  #get info of the model used here
+        pass
+
+    def voice():
+        pass   #Take arguements to select voices available in the engine.
+
+    def speak(text): #take arguements to speak.
+        pass
+
+    def __repr__():
+        pass
         
-            try:
-                os.system('espeak initialize')
-                return 'successfully initialize'
-           # assert os.system('echo $?') !
-            except:
-                return 'failed to initialize '
-        return 0        
-
-
-
-
+    def __dir__():
+        return [
+            'init',
+            'get_info',
+            'voice',
+            'speak'
+        ]
