0477b9ea48a143ec19ce551e52a8b2911d3fe0d3
black-scythe0 <scytheblack991@gmail.com>
2025-06-24 01:03:43 +0530

    can check, load, init and speak

--- pybstts/tts/_espeak.py
+++ pybstts/tts/_espeak.py
@@ -1,52 +1,62 @@
 import os
 import sys
 import ctypes
-from Exceptions import LibraryNotFound, LibraryFailedToLoad
-
+from ._Exceptions import (LibraryNotFound,
+                      LibraryFailedToLoad,
+                      )
 
 class Espeak:
-    def __init__():
-        pass
+    def __init__(self) -> None:
+        self.libespeak = None
+        self.lib_espeak_posix :str = 'libespeak-ng.so'
+        self.lib_path :str = ''
+        self.lib_paths = [
 
-    def check_lib():
-        global lib_espeak_posix
-        lib_espeak_posix='libespeak-ng.so'
-          
-        lib_path = [
+            # Termux
+            '/data/data/com.termux/files/usr/lib/libespeak-ng.so',
+            '/data/data/com.termux/files/usr/local/lib/libepeak-ng.so'
+        ]
 
-         # Termux
-        '/data/data/com.termux/files/usr/lib/libespeak-ng.so',
-        '/data/data/com.termux/files/usr/local/lib/libespeak-ng.so'     
-        
-            ]
+    def check_lib(self) -> int:
+        for lib_path in self.lib_paths:
+            if os.path.isfile(lib_path):
+                self.lib_path=lib_path    
+                return lib_path   
+
+        else:
+            raise LibraryNotFound(self.lib_espeak_posix) 
+
+    def load_lib(self) -> None:
+
         try:
-            for lib in lib_path:
-                if os.path.isfile(lib):
-                    return lib     # will return library path if found
-
-            return 1  # return 1 if not found               
+            self.libespeak = ctypes.cdll.LoadLibrary(self.lib_path)
         except:
-            raise LibraryNotFound(lib_espeak_posix)  # something unexpected  happen.
+            LibraryFailedToLoad(self.lib_espeak_posix)
+            
+    def lib_init(self, lang=None) -> None:
 
-    def load_library(lib):
-         try:
-             libespeak = ctypes.cdll.LoadLibrary(lib)
-             libespeak.espeak_Initialize.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.c_void_p]
-             libespeak.espeak_Initialize(0,0,None, None)
 
-             libespeak.espeak_SetVoiceByName(b'en')
-
-             def speak(text):
-                 text_bytes = text.encode('utf-8')
-                 libespeak.espeak_Synth(text_bytes, len(text_bytes), 0, 0, 0, 0, None, None)
-                 libespeak.espeak_Synchronize()
+        self.libespeak.espeak_Initialize.argtypes = [ctypes.c_int,
+                                                ctypes.c_int,
+                                                ctypes.c_char_p,
+                                                ctypes.c_void_p]
+                                       
+        self.libespeak.espeak_Initialize(0,0,None, None)
+        self.libespeak.espeak_SetVoiceByName(b'en')
+     
+    def speak(self,text: str) -> None:     
+        text_bytes = text.encode('utf-8')
+        self.libespeak.espeak_Synth(text_bytes, len(text_bytes), 0, 0, 0, 0, None, None)
+        self.libespeak.espeak_Synchronize()
                          
-                     
-          
-             speak("")
-             print("successfully loaded library")
-         except:
-             raise LibraryFailedToLoad(lib_espeak_posix)
 
 
+if __name__ == '__main__':
 
+    check = Espeak()
+
+    check.check_lib()
+    check.load_lib()
+    check.lib_init()
+    check.speak('hello')    
+    
