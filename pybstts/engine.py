0bd89a6aac892c677f672ee2fdfaa1a9db1e3cf3
black-scythe0 <scytheblack991@gmail.com>
2025-06-27 01:39:59 +0530

    some exceptions added bit get_info etc.

--- pybstts/engine.py
+++ pybstts/engine.py
@@ -1,34 +1,44 @@
 import os
 import importlib
-Exceptions = importlib.import_module(f'tts._Exceptions')
+from .checktts import TTS
+_Exceptions = importlib.import_module('pybstts.tts._Exceptions')
+
 
 class Engine:
 
-    def __init__(self, tts: str) -> None:
-        self.tts = tts
-        self._tts = importlib.import_module(f'tts.{tts}')
-        
+    def __init__(self, tts: str, TTS=TTS(), _Exceptions=_Exceptions) -> None:
+        self.tts :str = tts
+        self._tts = TTS.load_tts(tts)
+        self.tts_engine = None;        
 
-    def init(self) -> None:
+    def init(self) -> int:
         if self.tts == '_espeak':
-            self.tts = self._tts.Espeak()
-            self.tts.check_lib()
-            self.tts.load_lib()
-            self.tts.lib_init()
-                      
-
-    def get_info():  #get info of the model used here
-        pass
+            self.tts_engine = self._tts.Espeak()
+            self.tts_engine.check_lib()
+            self.tts_engine.load_lib()
+            self.tts_engine.lib_init()
+        else:
+            raise _Exceptions.EngineNotInitialized  
+        return 21
+    def get_info(self):  #get info of the model used here
+        return [
+        f'lib: pybstts', 
+        f'tts: {self.tts}'
+        ]
 
     def voice():
         pass   #Take arguements to select voices available in the engine.
 
     def speak(self, text: str | None) -> None: #take arguements to speak.
-
-        self.tts.speak(text) if text != None else None
-
-    def __repr__():
-        pass
+        try: 
+            if text != None :       
+                self.tts_engine.speak(text)
+            else:
+                pass            
+        except:
+            raise _Exceptions.EngineNotInitialized
+    def __repr__(self):
+        return '\'Engine\' from pybstts '
         
     def __dir__():
         return [
