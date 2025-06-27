154f95bca844ef28b08fc95e60a8881d162f1532
black-scythe0 <scytheblack991@gmail.com>
2025-06-27 19:37:16 +0530

    colours added

--- pybstts/tts/_Exceptions.py
+++ pybstts/tts/_Exceptions.py
@@ -1,32 +1,37 @@
+
+# Colours 
+yellow = '\033[93m'
+white = '\033[0m'
+
 class EngineNotInitialized(Exception):
-    def __init__(self, raise_msg='you forgot to initialize engine ?'):
-        self.raise_msg = raise_msg
-        super().__init__(f'{self.raise_msg}?')
+    def __init__(this, raise_msg='you forgot to initialize engine ?'):
+        this.raise_msg = raise_msg
+        super().__init__(f'{yellow}{this.raise_msg}?{white}')
 
 class EngineFailedToLoad(Exception):
-    def __init__(self, raise_msg='Engine failed to Load!'):
-        self.raise_msg = raise_msg
-        super().__init__(f'{self.raise_msg}')
+    def __init__(this, raise_msg='Engine failed to Load!'):
+        this.raise_msg = raise_msg
+        super().__init__(f'{yellow}{this.raise_msg}{white}')
 
 class EngineNotFound(Exception):
-    def __init__(self, raise_msg= 'Engine not found'):
-        self.raise_msg = raise_msg
-        super().__init__(f'{self.raise_msg}')
+    def __init__(this, raise_msg= 'Engine not found'):
+        this.raise_msg = raise_msg
+        super().__init__(f'{yellow}{this.raise_msg}{white}')
 
 
 class LibraryNotFound(Exception):
-    def __init__(self, lib, raise_msg= 'Not found library'):
-        self.raise_msg = raise_msg
-        self.lib = lib
-        super().__init__(f'{self.raise_msg}: {lib}')
+    def __init__(this, lib, raise_msg= 'Not found library'):
+        this.raise_msg = raise_msg
+        this.lib = lib
+        super().__init__(f'{yellow}{this.raise_msg}: {lib}{white}')
 
 
 
 class LibraryFailedToLoad(Exception):
-    def __init__(self, lib, raise_msg= 'Failed to load'):
-        self.raise_msg = raise_msg
-        self.lib =  lib
-        super().__init__(f'{self.raise_msg}: {lib}')
+    def __init__(this, lib, raise_msg= 'Failed to load'):
+        this.raise_msg = raise_msg
+        this.lib =  lib
+        super().__init__(f'{yellow}{this.raise_msg}: {lib}{white}')
 
 if __name__ == '__main__':
 #   raise LibraryFailedToLoad("library")
