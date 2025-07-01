9d91dd5e554b8195df3ca3c5caadece4ecdd3fd6
black-scythe0 <scytheblack991@gmail.com>
2025-07-01 23:04:51 +0530

    some changes

--- build.sh
+++ build.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/bash
 
+if [ $1 = "--clear-cache" ]; then
+    rm -rf pybstts/__pycache__/;
+    rm -rf pybstts/tts/__pycache__/;
+
+fi
+
 if [ -z $1 ]; then  
 
-python -m build
+    python -m build
 
 fi
 
 
 
-if [ $1 = "clean" ]; then
+if [ $1 = "--clean" ]; then
     rm -rf pybstts.egg-info dist/
 
+fi
+
+
+if [ $1 = "--publish" ]; then
+    python -m build 
+    
 fi
