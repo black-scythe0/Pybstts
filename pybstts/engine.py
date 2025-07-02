a2b0a069588849e165c01f24af1ee03151c6161f
black-scythe0 <scytheblack991@gmail.com>
2025-07-03 00:11:49 +0530

    added some doc-str, Exceptions only available in Engine scope

--- pybstts/engine.py
+++ pybstts/engine.py
@@ -1,46 +1,72 @@
-import os
 import importlib
 from .checktts import TTS
-_Exceptions = importlib.import_module('pybstts.tts._Exceptions')
+
 
 
 class Engine:
+    '''
+    pybstts Engine:
 
-    def __init__(this, tts: str, TTS=TTS(), _Exceptions=_Exceptions) -> None:
+    Methods:
+    init();       to initialize Engine
+    speak();      after initialization to speak.
+    get_info();   to get info about the tts
+    voice();      to change voice 
+    '''
+    def __init__(this,
+             tts: str,
+            TTS=TTS(),
+           _Exceptions= importlib
+                               .import_module('pybstts.tts._Exceptions')
+                  ) -> None:
+
         this.tts :str = tts
         this._tts = TTS.load_tts(tts)
         this.tts_engine = None;        
-
+        this._Exceptions = _Exceptions
     def init(this) -> None:
+        '''
+        initialize the Engine
+        '''
         if this.tts == '_espeak':
             this.tts_engine = this._tts.Espeak()
             this.tts_engine.check_lib()
             this.tts_engine.load_lib()
             this.tts_engine.lib_init()
         else:
-            raise _Exceptions.EngineNotInitialized  
+            raise this._Exceptions.EngineNotInitialized  
 
     def get_info(this):  #get info of the model used here
+        '''
+        get information about the tts.
+        '''
         return [
         f'lib: pybstts', 
         f'tts: {self.tts}'
         ]
 
-    def voice():
+    def voice(this):
         pass   #Take arguements to select voices available in the engine.
 
     def speak(this, text: str | None) -> None: #take arguements to speak.
+        '''
+        Takes string arguements to speak.
+
+        '''  
         try: 
             if text != None :       
                 this.tts_engine.speak(text)
             else:
                 pass            
         except: 
-            raise _Exceptions.EngineNotInitialized
+            raise this._Exceptions.EngineNotInitialized
     def __repr__(this):
-        return '\'Engine\' from pybstts '
+        '''
+        return string regarding Engine
+        '''
+        return '\'Engine\' from pybstts'
         
-    def __dir__():
+    def __dir__(this):
         return [
             'init',
             'get_info',
