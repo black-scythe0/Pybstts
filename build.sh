f2f5aaeedf5da3a40b4766977824ed47262872a2
Klaus10101 <klaus10101code@gmail.com>
2025-09-26 22:17:29 +0530

    some structural changes

--- build.sh
+++ build.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/bash
 
-if [ $1 = "--clean-cache" ]; then
-    rm -rf pybstts/__pycache__/;
-    rm -rf pybstts/tts/__pycache__/;
 
-fi
+case $1 in
+    --clean-cache)
+        rm -rf pybstts/__pycache__/;
+        rm -rf pybstts/tts/__pycache__/;
+    ;;
 
-if [ -z $1 ]; then  
-
-    python -m build
-
-fi
-
-
-
-if [ $1 = "--clean" ]; then
-    rm -rf pybstts.egg-info dist/
-
-fi
-
-
-if [ $1 = "--publish" ]; then
-    twine upload --repository testpypi dist/*
+    --clean) 
+        rm -rf pybstts.egg-info dist/
+    ;;
     
-fi
+    --publish)
+        twine upload --repository testpypi dist/*
+    ;;
+
+    *)
+        python3 -m build
+    ;;
+esac
